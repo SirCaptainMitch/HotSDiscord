75b0cb37342c2f57d53d90e36c7aac368849326a
SirCaptainMitch <hamann.mitchell@gmail.com>
2021-12-13 07:04:37 -0600

    added docker files, now pushes to server. also changing the bot name

--- initialize.ps1
+++
@@ -1,8 +0,0 @@
-param ( 
-  [switch]$Venv, 
-  [switch]$Install
-)
-
-
-if ( $Venv ) { python -m venv venv } 
-if ( $Install ) { python -m pip install -r .\requirements.txt }
--- test_sql.py
+++
@@ -1,4 +0,0 @@
-import pyodbc
-
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=192.168.0.245;DATABASE=lookingglass;UID=thecaptain;PWD=99Redbal00ns')
-cursor = cnxn.cursor()
